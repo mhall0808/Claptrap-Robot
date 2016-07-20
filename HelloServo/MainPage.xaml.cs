79eff04103b5269f312c10b529ee09313f3e7e2e
Mark Hall <mhall0717@yahoo.com>
2016-07-20 08:06:51 -0600

    Finished Product

--- HelloServo/MainPage.xaml.cs
+++ HelloServo/MainPage.xaml.cs
@@ -15,207 +15,104 @@ using Windows.UI.Xaml.Navigation;
 using Windows.Devices.Gpio;
 using System.Diagnostics;
 using Windows.Media.Capture;
+using System.Windows.Input;
+using Microsoft.IoT.Lightning.Providers;
+using Windows.Storage;
+using Windows.Media.Playback;
 
 namespace HelloServo
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
+        /**
+        * Each servo pin has a constant int declaring the 
+        * location of the pin, the actual GPIO pin of the servo
+        * pin, and a value containing the previous value of the pin.
+        * This value is being used in conjunction with the slider bar
+        * in order to successfully determine the direction of the servo
+        * RIGHT NOW, which there is no other indication of.
+        */
 
         private const int SERVO_PIN_A = 17;
         private const int SERVO_PIN_B = 27;
-        private const int SERVO_PIN_C = 22;
-        private const int SERVO_PIN_D = 10;
-        private const int SERVO_PIN_E = 9;
-        private const int SERVO_PIN_F = 11;
+        private const int SERVO_PIN_C = 5;
+        private const int SERVO_PIN_D = 6;
+
         private GpioPin servoPinA;
         private GpioPin servoPinB;
         private GpioPin servoPinC;
         private GpioPin servoPinD;
-        private GpioPin servoPinE;
-        private GpioPin servoPinF;
-        private DispatcherTimer timer;
-        private double BEAT_PACE = 1000;
-        private double CounterClockwiseDanceMove = 1;
-        private double ClockwiseDanceMove = 2;
-        private double currentDirection;
-        private double PulseFrequency = 20;
+
+        private int servoPrev1 = 0;
+        private int servoPrev2 = 0;
+        private int servoPrev3 = 0;
+        private int servoPrev4 = 0;
+
+        // Stopwatch is used so I can time my PWM pauses
         Stopwatch stopwatch;
+
+
+        // These are for the webcam.  
         MediaCaptureInitializationSettings captureInitSettings;
         List<Windows.Devices.Enumeration.DeviceInformation> deviceList;
         Windows.Media.MediaProperties.MediaEncodingProfile profile;
         Windows.Media.Capture.MediaCapture mediaCapture;
-
-
-
         GPIOout gpioOut;
+
+
         public MainPage()
         {
-            InitializeComponent();
-            gpioOut = new GPIOout();
-            EnumerateCameras();
-            //StartMediaCaptureSession();
-            //this.InitDancing();
-        }
+            GpioController gpioPin = GpioController.GetDefault();
 
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            gpioOut.forward();
-        }
-
-        private void button2_Click(object sender, RoutedEventArgs e)
-        {
-            gpioOut.turnRight();
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            gpioOut.turnLeft();
-        }
-
-        private void button4_Click(object sender, RoutedEventArgs e)
-        {
-            gpioOut.stop();
-        }
-
-        private void button3_Click(object sender, RoutedEventArgs e)
-        {
-            gpioOut.reverse();
-        }
-
-        private void button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-
-
-        private void InitDancing()
-        {
-            // Preparing our GPIO controller
-            var gpio = GpioController.GetDefault();
-
-            if (gpio == null)
+            if (gpioPin == null)
             {
-                servoPinA = null;
-                if (GpioStatus != null)
-                {
-                    GpioStatus.Text = "No GPIO controller found";
-                }
-
-                return;
             }
 
             // Servo set up
-            servoPinA = gpio.OpenPin(SERVO_PIN_A);
+            servoPinA = gpioPin.OpenPin(SERVO_PIN_A);
             servoPinA.SetDriveMode(GpioPinDriveMode.Output);
 
-            servoPinB = gpio.OpenPin(SERVO_PIN_B);
+            servoPinB = gpioPin.OpenPin(SERVO_PIN_B);
             servoPinB.SetDriveMode(GpioPinDriveMode.Output);
 
-            servoPinC = gpio.OpenPin(SERVO_PIN_C);
+            servoPinC = gpioPin.OpenPin(SERVO_PIN_C);
             servoPinC.SetDriveMode(GpioPinDriveMode.Output);
 
-            servoPinD = gpio.OpenPin(SERVO_PIN_D);
+            servoPinD = gpioPin.OpenPin(SERVO_PIN_D);
             servoPinD.SetDriveMode(GpioPinDriveMode.Output);
-
-            servoPinE = gpio.OpenPin(SERVO_PIN_E);
-            servoPinE.SetDriveMode(GpioPinDriveMode.Output);
-
-            servoPinF = gpio.OpenPin(SERVO_PIN_F);
-            servoPinF.SetDriveMode(GpioPinDriveMode.Output);
 
             stopwatch = Stopwatch.StartNew();
 
-            currentDirection = 0; // Initially we aren't dancing at all.
 
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(BEAT_PACE);
-            timer.Tick += Beat;
+            this.InitializeComponent();
 
-            if (servoPinA != null && servoPinB != null)
-            {
-                timer.Start();
-                Windows.System.Threading.ThreadPool.RunAsync(this.MotorThread, Windows.System.Threading.WorkItemPriority.High);
-            }
+            // Set up the DC motors
+            gpioOut = new GPIOout();
 
-            if (GpioStatus != null)
-            {
-                GpioStatus.Text = "GPIO pin ready";
-            }
+            // Set up the camera
+            EnumerateCameras();
         }
 
-        private void Beat(object sender, object e)
+
+        /**
+         * A snippet of code that I found on Stack Overflow.  Does not currently work, but it does compile.
+         * 
+         */
+        private async void playSound()
         {
-            if (currentDirection != ClockwiseDanceMove)
-            {
-                currentDirection = ClockwiseDanceMove;
-                GpioStatus.Text = "Yay!";
-            }
-            else
-            {
-                currentDirection = CounterClockwiseDanceMove;
-                GpioStatus.Text = "I'm Dancing!!";
-            }
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/BUMMER.wav"));
+            MediaPlayer player = BackgroundMediaPlayer.Current;
+            player.Volume = 1;
+            player.AutoPlay = false;
+            player.SetFileSource(file);
+            player.Play();
         }
-
-        private void MotorThread(IAsyncAction action)
-        {
-            while (true)
-            {
-                if (currentDirection != 0)
-                {
-                    servoPinA.Write(GpioPinValue.High);
-                    servoPinB.Write(GpioPinValue.High);
-                    servoPinC.Write(GpioPinValue.High);
-                    servoPinD.Write(GpioPinValue.High);
-                    servoPinE.Write(GpioPinValue.High);
-                    servoPinF.Write(GpioPinValue.High);
-                }
-
-                Wait(currentDirection);
-
-                servoPinA.Write(GpioPinValue.Low);
-                servoPinB.Write(GpioPinValue.Low);
-                servoPinC.Write(GpioPinValue.Low);
-                servoPinD.Write(GpioPinValue.Low);
-                servoPinE.Write(GpioPinValue.Low);
-                servoPinF.Write(GpioPinValue.Low);
-                Wait(PulseFrequency - currentDirection);
-            }
-        }
-
-        private void Wait(double milliseconds)
-        {
-            long initialTick = stopwatch.ElapsedTicks;
-            long initialElapsed = stopwatch.ElapsedMilliseconds;
-            double desiredTicks = milliseconds / 1000.0 * Stopwatch.Frequency;
-            double finalTick = initialTick + desiredTicks;
-            while (stopwatch.ElapsedTicks < finalTick)
-            {
-
-            }
-        }
-
-
-
-
-
-
-
 
 
         // Start the Video Capture
         private async void StartMediaCaptureSession()
         {
-            //var storageFile = await Windows.Storage.KnownFolders.VideosLibrary.CreateFileAsync("cameraCapture.wmv", Windows.Storage.CreationCollisionOption.GenerateUniqueName);
-            //fileName = storageFile.Name;
-
-            //await mediaCapture.StartRecordToStorageFileAsync(profile, storageFile);
-            //recording = true;
-
             // start the preview      
             capturePreview.Source = mediaCapture;
             await mediaCapture.StartPreviewAsync();
@@ -225,15 +122,12 @@ namespace HelloServo
         private async void StopMediaCaptureSession()
         {
             await mediaCapture.StopRecordAsync();
-            //recording = false;
-            //(App.Current as App).IsRecording = false;
-
             //stop the preview
             await mediaCapture.StopPreviewAsync();
-
-
         }
 
+
+        // Sets up the webcam for use.
         private async void EnumerateCameras()
         {
             var devices = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(Windows.Devices.Enumeration.DeviceClass.VideoCapture);
@@ -252,15 +146,14 @@ namespace HelloServo
             }
         }
 
+        // Sets up the capture settings.  We want just a streaming video, we don't need to record anything.
         private void InitCaptureSettings()
         {
             // Set the Capture Setting
             captureInitSettings = null;
             captureInitSettings = new Windows.Media.Capture.MediaCaptureInitializationSettings();
-            //captureInitSettings.AudioDeviceId = "";
             captureInitSettings.VideoDeviceId = "";
             captureInitSettings.StreamingCaptureMode = Windows.Media.Capture.StreamingCaptureMode.Video;
-            //captureInitSettings.PhotoCaptureSource = Windows.Media.Capture.PhotoCaptureSource.VideoPreview;
 
             if (deviceList.Count > 0)
             {
@@ -269,34 +162,21 @@ namespace HelloServo
 
         }
 
+        // Initializes the media capture function.
         private async void InitMediaCapture()
         {
             mediaCapture = null;
             mediaCapture = new Windows.Media.Capture.MediaCapture();
 
-            // for dispose purpose
-            //(App.Current as App).MediaCapture = mediaCapture;
-            //(App.Current as App).PreviewElement = capturePreview;
 
             await mediaCapture.InitializeAsync(captureInitSettings);
 
             // Add video stabilization effect during Live Capture
-            // await _mediaCapture.AddEffectAsync(MediaStreamType.VideoRecord, Windows.Media.VideoEffects.VideoStabilization, null); //this will be deprecated soon
             Windows.Media.Effects.VideoEffectDefinition def = new Windows.Media.Effects.VideoEffectDefinition(Windows.Media.VideoEffects.VideoStabilization);
 
             await mediaCapture.AddVideoEffectAsync(def, MediaStreamType.VideoPreview);
 
             CreateProfile();
-
-            // start preview
-
-            //capturePreview.Source = mediaCapture;
-
-            //DisplayInformation.AutoRotationPreferences = DisplayOrientations.None;
-
-            //// set the video Rotation
-            //  _mediaCapture.SetPreviewRotation(VideoRotation.Clockwise90Degrees);
-            //    _mediaCapture.SetRecordRotation(VideoRotation.Clockwise90Degrees);
         }
 
         //Create a profile
@@ -312,10 +192,9 @@ namespace HelloServo
             // add the mediaTranscoder 
             //var transcoder = new Windows.Media.Transcoding.MediaTranscoder();
             //transcoder.AddVideoEffect(Windows.Media.VideoEffects.VideoStabilization);
-
-
-
         }
+
+        // Rotation of the camera
         int ConvertVideoRotationToMFRotation(VideoRotation rotation)
         {
             int MFVideoRotation = 0;
@@ -334,29 +213,229 @@ namespace HelloServo
             return MFVideoRotation;
         }
 
+        // Stops the capture session
         private void stopCapture(object sender, RoutedEventArgs e)
         {
             StopMediaCaptureSession();
         }
 
+        // begins the capture session
         private void beginSession_Click(object sender, RoutedEventArgs e)
         {
             StartMediaCaptureSession();
         }
 
-        // open the file in stream here
+        /**
+         * GPIO Button Click Events
+         * 
+         *  These click events have been simplified to simply turn on or off.
+         *  Because DC motors simply require a pin value, this fits perfectly
+         *  with our design.
+         */
+
+        // Forward
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            gpioOut.forward();
+        }
+
+        // Turn Right
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            gpioOut.turnRight();
+
+        }
+
+        // Turn Left
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            gpioOut.turnLeft();
+
+        }
+
+        // Reverse
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            gpioOut.reverse();
+
+        }
+
+        // Stop all pins
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            gpioOut.stop();
+        }
+
+
+        /**
+         * SERVO PIN CHANGE VALUE
+         * 
+         * Each servo pin is attached to a slider bar.  This slider bar is set to rotate X amount, where X is where the
+         * user tells it to go.  This is a pulse width modulator:
+         * 
+         * The user turns the pin on high.  1 ms wait time makes the unit turn clockwise.  2 ms wait time makes the 
+         * unit turn counter-clockwise.  A 20 ms low wait time is required after every "high" cycle, allowing the servo
+         * to move.  to continue moving, you simply cycle back and keep rotating the servo until satisfied.
+         * 
+         * In our case, the math is done by moving the slider bar and comparing the older values with the new one.  It then
+         * determines which direction to go and makes its move.
+         * 
+         */
+        private void leftShoulder_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+
+            int toSend = (int)leftShoulder.Value - servoPrev1;
+            servoPrev1 = (int)leftShoulder.Value;
+
+
+            for (int i = 0; i < Math.Abs(toSend) / 2; i++)
+            {
+
+                if (toSend > 0)
+                {
+                    servoPinA.Write(GpioPinValue.High);
+                    Wait(1);
+                    servoPinA.Write(GpioPinValue.Low);
+                    Wait(19);
+                }
+                else if (toSend < 0)
+                {
+                    servoPinA.Write(GpioPinValue.High);
+                    Wait(2);
+                    servoPinA.Write(GpioPinValue.Low);
+                    Wait(18);
+                }
+
+
+            }
+
+
+
+        }
+
+        private void leftForearm_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+
+
+
+            int toSend = (int)leftForearm.Value - servoPrev2;
+            servoPrev2 = (int)leftForearm.Value;
+
+
+            for (int i = 0; i < Math.Abs(toSend) / 2; i++)
+            {
+
+                if (toSend > 0)
+                {
+                    servoPinB.Write(GpioPinValue.High);
+                    Wait(1);
+                    servoPinB.Write(GpioPinValue.Low);
+                    Wait(19);
+                }
+                else if (toSend < 0)
+                {
+                    servoPinB.Write(GpioPinValue.High);
+                    Wait(2);
+                    servoPinB.Write(GpioPinValue.Low);
+                    Wait(18);
+                }
+
+
+            }
+
+
+        }
+
+
+
+        private void rightShoulder_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
 
 
 
 
 
+            int toSend = (int)rightShoulder.Value - servoPrev3;
+            servoPrev3 = (int)rightShoulder.Value;
+
+
+            for (int i = 0; i < Math.Abs(toSend) / 2; i++)
+            {
+
+                if (toSend > 0)
+                {
+                    servoPinC.Write(GpioPinValue.High);
+                    Wait(1);
+                    servoPinC.Write(GpioPinValue.Low);
+                    Wait(19);
+                }
+                else if (toSend < 0)
+                {
+                    servoPinC.Write(GpioPinValue.High);
+                    Wait(2);
+                    servoPinC.Write(GpioPinValue.Low);
+                    Wait(18);
+                }
+
+
+            }
+
+
+        }
+
+        private void rightForearm_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+
+
+            int toSend = (int)rightForearm.Value - servoPrev4;
+            servoPrev4 = (int)rightForearm.Value;
+
+
+            for (int i = 0; i < Math.Abs(toSend) / 2; i++)
+            {
+
+                if (toSend > 0)
+                {
+                    servoPinD.Write(GpioPinValue.High);
+                    Wait(1);
+                    servoPinD.Write(GpioPinValue.Low);
+                    Wait(19);
+                }
+                else if (toSend < 0)
+                {
+                    servoPinD.Write(GpioPinValue.High);
+                    Wait(2);
+                    servoPinD.Write(GpioPinValue.Low);
+                    Wait(18);
+                }
+
+
+            }
+
+
+        }
 
 
 
+        /**
+         * WAIT FUNCTION
+         * 
+         * Apparently you can't just set it to Thread.Sleep(), so this is necessary.  It is a simple
+         * ticker event that counts down and shoves  you in a for loop.  I am planning on optimizing and
+         * getting rid of this later.
+         * 
+         */
+        private void Wait(double milliseconds)
+        {
+            long initialTick = stopwatch.ElapsedTicks;
+            long initialElapsed = stopwatch.ElapsedMilliseconds;
+            double desiredTicks = milliseconds / 1000.0 * Stopwatch.Frequency;
+            double finalTick = initialTick + desiredTicks;
+            while (stopwatch.ElapsedTicks < finalTick)
+            {
 
-
-
-
+            }
+        }
 
 
     }
